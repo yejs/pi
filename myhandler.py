fd836632bdbfbdb6dfbf67c124e484e82211f0e6
yejs <100ye@163.com>
2016-05-20 16:47:51 +0800

    update

--- myhandler.py
+++ myhandler.py
@@ -42,13 +42,11 @@ class RPi_GPIO():
 		if RPi_GPIO._is_exist:
 			io.cleanup()
 	
-	def lamp(channle, command):
+	def output(channle, command):
 		if RPi_GPIO._is_exist == False:
 			return;
-		if command == 'on':
-			io.output(int(channle), io.LOW)
-		elif command == 'off':
-			io.output(int(channle), io.HIGH)
+
+		io.output(channle, command)
 
 class ControlHandler(tornado.web.RequestHandler):
     def post(self, *args, **kwargs):
@@ -58,21 +56,48 @@ class ControlHandler(tornado.web.RequestHandler):
         ''' 
         post 参数示例: /control?dev_id=lamp&id=1&command=on
         '''
-        str = '{\"dev_id\":\"'+post_data['dev_id'][0]+'\", \"id\":\"'+post_data['id'][0]+'\", \"command\":\"'+post_data['command'][0]+'\"}';
-        #print ("do_POST:",  _OUT_[post_data['id'][0]])
+        if post_data['dev_id'][0] == 'lamp':
+            str = '{\"dev_id\":\"'+post_data['dev_id'][0]+'\", \"id\":\"'+post_data['id'][0]+'\", \"command\":\"'+post_data['command'][0]+'\"}';
+        elif post_data['dev_id'][0] == 'car':
+            str = '{\"dev_id\":\"'+post_data['dev_id'][0]+'\", \"command\":\"'+post_data['command'][0]+'\"}'; 
+			
         self.write(base64.encodestring(str.encode('gbk')))#响应页面post请求（数据base64简单加密处理）
 		
         if post_data['dev_id'][0] == 'lamp':
-            self.lampHandler(post_data)
+            ControlHandler.lamp(post_data)
+        elif post_data['dev_id'][0] == 'car':
+            ControlHandler.car(post_data)
 					
-    def lampHandler(self, post_data):
+    def lamp(post_data):
         id = post_data['id'][0]
-        command = post_data['command'][0]
-        RPi_GPIO.lamp(_OUT_[id], command)
+        if post_data['command'][0] == 'on':	
+            command = False
+        elif post_data['command'][0] == 'off':
+            command = True
+        RPi_GPIO.output(int(_OUT_[id]), command)
 
         if id == 12:
             for k,v in _OUT_:
-                RPi_GPIO.lamp(v, command)
+                RPi_GPIO.output(int(v), command)
+				
+    def car(post_data):
+        command = post_data['command'][0]
+        _CAR_ = {'INT1':11, 'INT2':12, 'INT3':13, 'INT4':15}
+        if command == 'front':
+            RPi_GPIO.output(_CAR_['INT1'], True)
+            RPi_GPIO.output(_CAR_['INT2'], False)
+            RPi_GPIO.output(_CAR_['INT3'], True)
+            RPi_GPIO.output(_CAR_['INT4'], False)
+        elif command == 'back':
+            RPi_GPIO.output(_CAR_['INT1'], False)
+            RPi_GPIO.output(_CAR_['INT2'], True)
+            RPi_GPIO.output(_CAR_['INT3'], False)
+            RPi_GPIO.output(_CAR_['INT4'], True)
+        elif command == 'stop':
+            RPi_GPIO.output(_CAR_['INT1'], False)
+            RPi_GPIO.output(_CAR_['INT2'], False)
+            RPi_GPIO.output(_CAR_['INT3'], False)
+            RPi_GPIO.output(_CAR_['INT4'], False)
 
 if __name__ == "__main__":
     pass
