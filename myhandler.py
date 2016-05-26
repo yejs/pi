ff7698c7dbedec344e3d5c7800a658bade40d274
yejs <baiyesoft@126.com>
2016-05-26 22:40:17 +0800

    add

--- myhandler.py
+++ myhandler.py
@@ -9,7 +9,7 @@ __version__ = '1.0'
 __all__ = ["ControlHandler", "RPi_GPIO"]
 
 import os
-#import RPi.GPIO as io
+import RPi.GPIO as io
 import tornado.web
 import json
 import base64
@@ -17,6 +17,7 @@ import base64
 #BOARD模式
 _OUT_ = {'1':'11', '2':'12', '3':'13', '4':'15', '5':'16', '6':'18', '7':'22', '8':'7', '9':'29', '10':'31', '11':'33', '12':'35', '13':'37', '14':'32', '15':'36', '16':'38', '17':'40'}
 
+_LAMP_ = {'1':'off', '2':'off', '3':'off', '4':'off', '5':'off', '6':'off', '7':'off', '8':'off', '9':'off', '10':'off', '11':'off', '12':'off', '13':'off', '14':'off', '15':'off', '16':'off', '17':'off'}
 
 
 class RPi_GPIO():
@@ -56,13 +57,16 @@ class ControlHandler(tornado.web.RequestHandler):
         post 参数示例: /control?dev_id=lamp&id=1&command=on
         '''
 
-        str = '{\"dev_id\":\"'+post_data['dev_id'][0]+'\", \"id\":\"'+post_data['id'][0]+'\", \"command\":\"'+post_data['command'][0]+'\"}';
-        self.write(base64.encodestring(str.encode('gbk')))#响应页面post请求（数据base64简单加密处理）
-		
+
         if post_data['dev_id'][0] == 'lamp':
             ControlHandler.lamp(post_data)
+            
+            #str1 = '{\"dev_id\":\"'+post_data['dev_id'][0]+'\", \"id\":\"'+post_data['id'][0]+'\", \"command\":\"'+post_data['command'][0]+'\", \"status\":\"'+str(_LAMP_)+'\"}'
         elif post_data['dev_id'][0] == 'car':
-            ControlHandler.car(post_data)
+            ControlHandler.car(post_data) 
+			
+        str1 = '{\"dev_id\":\"'+post_data['dev_id'][0]+'\", \"id\":\"'+post_data['id'][0]+'\", \"command\":\"'+post_data['command'][0]+'\"}'			
+        self.write(base64.encodestring(str1.encode('gbk')))#响应页面post请求（数据base64简单加密处理）
 					
     def lamp(post_data):
         id = post_data['id'][0]
@@ -75,6 +79,9 @@ class ControlHandler(tornado.web.RequestHandler):
         if id == '12':
             for k,v in _OUT_.items():
                 RPi_GPIO.output(int(v), command)
+                _LAMP_[k] = post_data['command'][0]
+        else:
+            _LAMP_[post_data['id'][0]] = post_data['command'][0]
 				
     def car(post_data):
         command = post_data['command'][0]
