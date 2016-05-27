dd9c332c1eda6195f1f3fa496aae417312028cb7
yejs <100ye@163.com>
2016-05-27 17:56:30 +0800

    add

--- ip.py
+++ ip.py
@@ -8,12 +8,13 @@ for I in res:
         print(I[:18])
 		
 		#curl "https://raw.github.com/ma6174/speak_raspi_ip/master/setup.sh" | bash
-		
 		#http://bbs.elecfans.com/forum.php?mod=viewthread&tid=440126 基于motion视频捕捉软件的树莓派视频拍照系统
-		
 		#http://www.cnblogs.com/peida/archive/2013/03/25/2980121.html 自动删除n天前日志
 		
-		'''
+		#http://blog.csdn.net/bona020/article/details/51034043#comments
+		#http://blog.csdn.net/Bobsweetie/article/details/50814849
+		#http://blog.sina.com.cn/s/blog_abd39cc70102vrdt.html
+		''' 
 		v4l2_palette 8
 		minimum_frame_time 1
 		'''
